ad6e36f68c7608685f0a032524e198c12b552115
mahedi <mahedianik4@gmail.com>
2022-03-21 21:59:51 +0600

    nav bar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,15 @@ import logo from '../../images/Logo.svg'
 import './Header.css'
 const Header = () => {
     return (
-        <div className='nav-header'>
-            <nav>
-                <img src={logo} alt="" />
-
-            </nav>
-            
-        </div>
+        <nav className='nav-header'>
+                <img src={logo} alt="emajohnlogo" />
+                <div>
+                    <a href="/Shop">Shop</a>
+                    <a href="/Order">Order</a>
+                    <a href="/Order review"> Order review</a>
+                    <a href="/Manage Inventory">Manage Inventory</a>
+                </div>        
+        </nav>
     );
 };
 
