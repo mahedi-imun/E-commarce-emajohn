1b7f1f19bf9069e5ba66be2dd6c7f9bc34d0b6c1
mahedi <mahedianik4@gmail.com>
2022-03-23 01:26:16 +0600

    total price , tax, total shipping, and grand total celculate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({cart}) => {
+    let total =0;
+    let shippingCost =0
+    let tax = 0;
+    let grandTotal =0
+    for(let product of cart){
+        total = total+product.price
+        shippingCost = shippingCost +product.shipping;
+        const newTax=(total *0.1).toFixed(2);
+        tax =parseFloat(newTax)
+        grandTotal = total +shippingCost+tax
+    };
+
     return (
         <div className='cart'>
             <h3 className='cart-header'>Order summary</h3>
             <div className='cart-body'>
-            <h4>Selected Items: {cart.length}</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Shipping cost: ${shippingCost}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
             </div>
         </div>
     );
