fb64f86b1e48bc9052e376aefc63fa6887753af5
mahedi <mahedianik4@gmail.com>
2022-03-24 01:03:14 +0600

    fixed cart celculate , set item local storage and get data local storage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -25,10 +25,21 @@ const Shop = () => {
         setCart(savedCart)
 
     }, [products])
-    const addToCartHandler = (product) => {
-        const newCart = [...cart, product]
+    const addToCartHandler = (selectedProduct) => {
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        let newCart = []
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+
         setCart(newCart)
-        addToDb(product.id)
+        addToDb(selectedProduct.id)
     }
     return (
         <div className='shop-container'>
