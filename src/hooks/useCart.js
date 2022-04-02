192202f97b66a7f770e5506f14ec8afa267a550a
mahedi <mahedianik4@gmail.com>
2022-04-02 23:33:16 +0600

    add redirects file for netlify

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,23 +1,20 @@
 import { useEffect, useState } from "react"
 import { getLocalStorageItem } from "../utilities/fakedb"
-
 const useCart = (products) => {
-
     const [cart, setCart] = useState([])
-   useEffect(()=>{
-    const getStorageCart = getLocalStorageItem();
-    const savedCart = [];
-    for(let  id in getStorageCart){
-        const addedProduct = products.find(product => product.id === id);
-        if(addedProduct){
-            const quantity = getStorageCart[id]
-            addedProduct.quantity = quantity
-            savedCart.push(addedProduct)
+    useEffect(() => {
+        const getStorageCart = getLocalStorageItem();
+        const savedCart = [];
+        for (let id in getStorageCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = getStorageCart[id]
+                addedProduct.quantity = quantity
+                savedCart.push(addedProduct)
+            }
         }
-
-    }
-    setCart(savedCart)
-   },[products])
-   return [cart ,setCart]
+        setCart(savedCart)
+    }, [products])
+    return [cart, setCart]
 }
 export default useCart
